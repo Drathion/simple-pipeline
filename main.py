5745468334b908f81accf33387278a43ba70c676
Drathion <drathion@outlook.com>
2023-01-08 01:41:31 -0500

    fleshed out main.py web server

--- main.py
+++ main.py
@@ -7,9 +7,19 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
+@app.route('/', methods=['GET'])
+def hello():
     """
-    Main Function for home page of web app to display the test "Hello, World!!!"
+    hello Function for home page of web app to display the test "Hello, World!!!"
     """
     return 'Hello, World!!!'
+
+
+def main():
+    """
+    Function that runs web server
+    """
+    app.run(host='0.0.0.0', port=8080)
+
+if __name__ == '__main__':
+    main()
